5afd58a305d5c0bc1ab45fe291ee087dd829df1d
鄭洽棟 <crashxz1023@qq.com>
2022-02-14 20:56:54 +0800

    提交search

---
+++ pages/index/classification.vue
@@ -0,0 +1,56 @@
+<template>
+
+	<view class="tab">
+
+		<Goods v-for="(item,index) in searchData" :goodsList="item"></Goods>
+
+
+	</view>
+
+
+</template>
+
+<script>
+	import Goods from '../../components/good.vue';
+	import {
+		fetchSearch
+	} from '../../api/index.js'
+	export default {
+		components: {
+			Goods
+		},
+		data() {
+			return {
+
+			};
+		},
+		methods: {
+			switchover: function(id) {
+				console.log(id)
+			},
+			async search(page, size, keyWord) {
+				let {
+					data
+				} = await fetchSearch(page, size, keyWord)
+				this.searchData = data.list
+				console.log(this.searchData)
+			}
+		},
+		created() {
+
+		},
+		onLoad(openid) {
+			this.search(1, 10, openid.keyWord)
+
+		}
+	}
+</script>
+
+<style scoped>
+	.tab {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		background-color: #f2f2f2;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="content">
+		<view>
+			<zy-search :is-focus="false" :theme="themeClass" :show-want="true" :speechEngine="speechEngine"
+				:hot-list="hotList" @getSearchText="getSearchText"></zy-search>
+		</view>
+	</view>
+</template>
+
+<script>
+	import zySearch from '../../components/zy-search/zy-search.vue'
+	import {fetchSearch} from '../../api/index.js'
+	export default {
+		components: {
+			zySearch
+		},
+		data() {
+			return {
+				title: 'Hello',
+				themeClass: 'block',
+				speechEngine: 'baidu', //语音识别引擎
+				hotList: ['口红', '洗面奶', '身体乳'] //初始化推荐列表
+			}
+		},
+		methods: {
+			getSearchText(e) {
+				uni.showToast({
+					title: '搜索信息: ' + e,
+					icon: "none"
+				}),
+				console.log(e);
+				
+			}
+			
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
