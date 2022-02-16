c27c6fe8b4d942074df9e41aa75cd5f5b98b605a
小创 <73729378+jian-chuang@users.noreply.github.com>
2022-02-16 21:41:27 +0800

    feat:新增拦截器

--- api/request.js
+++ api/request.js
@@ -1,14 +1,60 @@
 module.exports = function(options) {
 	return new Promise((reslove, reject) => {
-		uni.request({
+		uni.showLoading({
+			title: 'loading...'
+		})
+		let httpRequest = {
 			...options,
-			success:(res)=>{
+			success: (res) => {
 				reslove(res.data)
 			},
-			fail: reject,
-			complete: (res) => {}
-		})
+			fail: (err) => {
+				reject(err)
+			},
+			complete: (res) => {
+				uni.hideLoading()
+				let {
+					code,
+					message
+				} = res.data
+				if (code === 10004 || code === 10005) {
+					uni.showToast({
+						title: message
+					})
+					uni.redirectTo({
+						// #ifdef MP-WEIXIN
+						url: '../../pages/login/login',
+						// #endif
+						// #ifdef H5
+						url: '../../pages/login/h5login',
+						// #endif
+					})
+				}
+			}
+		}
+		const reg = /\/user\//
+		let result = reg.test(options.url)
+		// 需要权限
+		if (result) {
+			let token = uni.getStorageSync('token')
+			if (token === '') {
+				uni.redirectTo({
+					// #ifdef MP-WEIXIN
+					url: '../../pages/login/login',
+					// #endif
+					// #ifdef H5
+					url: '../../pages/login/h5login',
+					// #endif
+				})
+				return
+			}
+			httpRequest.header = {
+				Authorization: token
+			}
+			uni.request(httpRequest)
+		} else {
+			uni.request(httpRequest)
+		}
+
 	})
 }
-
-
